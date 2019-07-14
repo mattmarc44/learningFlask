98ec9a93a46e0e819ed2232b50334f3fd73a505e
Matt Titmuss <45769440+mattmarc44@users.noreply.github.com>
2019-07-14 16:41:14 +0100

    using python variables to dynamically add content, use for loop in the html to repeat content from those variables all coming from a json file

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title="Home | Coffee")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/coffee.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About | Coffee", coffee=data)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact | Coffee")
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers | Coffee")
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
